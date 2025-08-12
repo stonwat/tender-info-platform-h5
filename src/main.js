82f0367dc7d7b09249df02e509d63067c7e2dac4
Vanze <stonwat@163.com>
2025-08-12 13:57:35 +0800

    文件修改

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 // 引入 Tailwind（确保 tailwind.config.js 正确）
-import './styles/tailwind.css'
+import './assets/styles/tailwind.css'
 
 // 引入 font-awesome 样式
 import 'font-awesome/css/font-awesome.min.css'
